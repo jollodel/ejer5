091560eed719c190f90c9017ad91888e73dea2f2
Jose Manuel <jollodel@hotmail.com>
2020-01-09 15:35:34 +0100

    first committ

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  APIENDPOINT_BACKEND: 'http://192.168.0.60:3000'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  APIENDPOINT_BACKEND: 'http://localhost:3000'
 };
 
 /*
